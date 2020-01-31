695017028062d981fb95a4ea64b67a737a5e9884
emreturgutce <emrebursalee@gmail.com>
2020-02-01 00:49:38 +0300

    Removed unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Client side js is loaded.');
-
 const form = document.querySelector('form');
 const input = document.querySelector('input');
 const error = document.querySelector('#error');
